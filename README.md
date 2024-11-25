870c99b3d5c4e9b8b7abfc6a35235d93e6d7ebe5
Abder-hbt <9907@holbertonstudents.com>
2024-11-25 15:28:57 +0000

    project creation de la fonction print f

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-s
+svdfvdf
--- printf.c
+++ printf.c
@@ -1,24 +1,57 @@
 #include <stdarg.h>
+#include <stdio.h>
 #include <unistd.h>
-#include "printf.h"
+#include "main.h"
+
 int _printf(const char *format, ...)
 {
     int i;
     va_list args;
 
-    if(format == NULL)
+    if (format == NULL)
     {
         return (-1);
     }
 
-    for( i = 0; format[i] != '\0'; i++)
+    va_start(args, format);
+
+    for (i = 0; format[i] != '\0'; i++)
     {
-        if(format[i] == '%'){
-           _putchar('%');
-        } else 
+        if (format[i] == '%')
         {
-            _putchar(format[i]);
+            i++;
+
+            if (format[i] == 'c')
+            {
+                char c = va_arg(args, int);
+                putchar(c);
+            }
+            else if (format[i] == 's') 
+            {
+                char *str = va_arg(args, char*);
+                while (*str)
+                {
+                    putchar(*str);
+                    str++;
+                }
+            }
+            else if (format[i] == '%')
+            {
+                putchar('%');
+            }
+            else
+            {
+                putchar('%');
+                putchar(format[i]);
+            }
         }
-        
+        else
+        {
+            putchar(format[i]);
+        }
     }
+
+    va_end(args);
+
+    return 0;
 }
